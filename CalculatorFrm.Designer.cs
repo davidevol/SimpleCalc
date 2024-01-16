3266505bacfcee05b3e93adb28659a996af5de97
De <deehzi@pm.me>
2024-01-16 19:47:49 -0300

    Add project files.

---
+++ CalculatorFrm.Designer.cs
@@ -0,0 +1,372 @@
+﻿namespace SimpleCalc
+{
+    partial class CalculatorFrm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CalculatorFrm));
+            main_textBox = new TextBox();
+            button_7 = new Button();
+            button_8 = new Button();
+            button_9 = new Button();
+            button_div = new Button();
+            button_mult = new Button();
+            button_6 = new Button();
+            button_5 = new Button();
+            button_4 = new Button();
+            button_minus = new Button();
+            button_3 = new Button();
+            button_2 = new Button();
+            button_1 = new Button();
+            button_plus = new Button();
+            button_erase = new Button();
+            button_0 = new Button();
+            button_decimal = new Button();
+            button_equal = new Button();
+            clear_button = new Button();
+            label_first_number = new Label();
+            label_operator = new Label();
+            toolTip_read_number = new ToolTip(components);
+            SuspendLayout();
+            // 
+            // main_textBox
+            // 
+            main_textBox.AcceptsReturn = true;
+            main_textBox.AcceptsTab = true;
+            main_textBox.BorderStyle = BorderStyle.FixedSingle;
+            main_textBox.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            main_textBox.Location = new Point(25, 45);
+            main_textBox.MaxLength = 100;
+            main_textBox.Name = "main_textBox";
+            main_textBox.ReadOnly = true;
+            main_textBox.ScrollBars = ScrollBars.Vertical;
+            main_textBox.Size = new Size(423, 61);
+            main_textBox.TabIndex = 0;
+            main_textBox.Text = "0";
+            main_textBox.TextAlign = HorizontalAlignment.Center;
+            main_textBox.TextChanged += main_textBox_TextChanged;
+            main_textBox.MouseLeave += main_textBox_MouseLeave;
+            main_textBox.MouseHover += main_textBox_MouseHover;
+            // 
+            // button_7
+            // 
+            button_7.Cursor = Cursors.Hand;
+            button_7.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button_7.Location = new Point(11, 233);
+            button_7.Name = "button_7";
+            button_7.Size = new Size(105, 88);
+            button_7.TabIndex = 1;
+            button_7.Text = "7";
+            button_7.UseVisualStyleBackColor = true;
+            button_7.Click += button_7_Click;
+            // 
+            // button_8
+            // 
+            button_8.Cursor = Cursors.Hand;
+            button_8.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button_8.Location = new Point(127, 233);
+            button_8.Name = "button_8";
+            button_8.Size = new Size(105, 88);
+            button_8.TabIndex = 2;
+            button_8.Text = "8";
+            button_8.UseVisualStyleBackColor = true;
+            button_8.Click += button_8_Click;
+            // 
+            // button_9
+            // 
+            button_9.Cursor = Cursors.Hand;
+            button_9.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button_9.Location = new Point(243, 233);
+            button_9.Name = "button_9";
+            button_9.Size = new Size(105, 88);
+            button_9.TabIndex = 3;
+            button_9.Text = "9";
+            button_9.UseVisualStyleBackColor = true;
+            button_9.Click += button_9_Click;
+            // 
+            // button_div
+            // 
+            button_div.Cursor = Cursors.Hand;
+            button_div.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button_div.Image = (Image)resources.GetObject("button_div.Image");
+            button_div.Location = new Point(359, 233);
+            button_div.Name = "button_div";
+            button_div.Size = new Size(105, 88);
+            button_div.TabIndex = 4;
+            button_div.UseVisualStyleBackColor = true;
+            button_div.Click += button_div_Click;
+            // 
+            // button_mult
+            // 
+            button_mult.Cursor = Cursors.Hand;
+            button_mult.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button_mult.Image = (Image)resources.GetObject("button_mult.Image");
+            button_mult.Location = new Point(359, 333);
+            button_mult.Name = "button_mult";
+            button_mult.Size = new Size(105, 88);
+            button_mult.TabIndex = 8;
+            button_mult.UseVisualStyleBackColor = true;
+            button_mult.Click += button_mult_Click;
+            // 
+            // button_6
+            // 
+            button_6.Cursor = Cursors.Hand;
+            button_6.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button_6.Location = new Point(243, 333);
+            button_6.Name = "button_6";
+            button_6.Size = new Size(105, 88);
+            button_6.TabIndex = 7;
+            button_6.Text = "6";
+            button_6.UseVisualStyleBackColor = true;
+            button_6.Click += button_6_Click;
+            // 
+            // button_5
+            // 
+            button_5.Cursor = Cursors.Hand;
+            button_5.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button_5.Location = new Point(127, 333);
+            button_5.Name = "button_5";
+            button_5.Size = new Size(105, 88);
+            button_5.TabIndex = 6;
+            button_5.Text = "5";
+            button_5.UseVisualStyleBackColor = true;
+            button_5.Click += button_5_Click;
+            // 
+            // button_4
+            // 
+            button_4.Cursor = Cursors.Hand;
+            button_4.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button_4.Location = new Point(11, 333);
+            button_4.Name = "button_4";
+            button_4.Size = new Size(105, 88);
+            button_4.TabIndex = 5;
+            button_4.Text = "4";
+            button_4.UseVisualStyleBackColor = true;
+            button_4.Click += button_4_Click;
+            // 
+            // button_minus
+            // 
+            button_minus.Cursor = Cursors.Hand;
+            button_minus.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button_minus.Location = new Point(359, 435);
+            button_minus.Name = "button_minus";
+            button_minus.Size = new Size(105, 88);
+            button_minus.TabIndex = 12;
+            button_minus.Text = "-";
+            button_minus.UseVisualStyleBackColor = true;
+            button_minus.Click += button_minus_Click;
+            // 
+            // button_3
+            // 
+            button_3.Cursor = Cursors.Hand;
+            button_3.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button_3.Location = new Point(243, 435);
+            button_3.Name = "button_3";
+            button_3.Size = new Size(105, 88);
+            button_3.TabIndex = 11;
+            button_3.Text = "3";
+            button_3.UseVisualStyleBackColor = true;
+            button_3.Click += button_3_Click;
+            // 
+            // button_2
+            // 
+            button_2.Cursor = Cursors.Hand;
+            button_2.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button_2.Location = new Point(127, 435);
+            button_2.Name = "button_2";
+            button_2.Size = new Size(105, 88);
+            button_2.TabIndex = 10;
+            button_2.Text = "2";
+            button_2.UseVisualStyleBackColor = true;
+            button_2.Click += button_2_Click;
+            // 
+            // button_1
+            // 
+            button_1.Cursor = Cursors.Hand;
+            button_1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button_1.Location = new Point(11, 435);
+            button_1.Name = "button_1";
+            button_1.Size = new Size(105, 88);
+            button_1.TabIndex = 9;
+            button_1.Text = "1";
+            button_1.UseVisualStyleBackColor = true;
+            button_1.Click += button_1_Click;
+            // 
+            // button_plus
+            // 
+            button_plus.Cursor = Cursors.Hand;
+            button_plus.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button_plus.Image = (Image)resources.GetObject("button_plus.Image");
+            button_plus.Location = new Point(359, 534);
+            button_plus.Name = "button_plus";
+            button_plus.Size = new Size(105, 88);
+            button_plus.TabIndex = 16;
+            button_plus.UseVisualStyleBackColor = true;
+            button_plus.Click += button_plus_Click;
+            // 
+            // button_erase
+            // 
+            button_erase.Cursor = Cursors.Hand;
+            button_erase.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button_erase.Image = (Image)resources.GetObject("button_erase.Image");
+            button_erase.Location = new Point(243, 534);
+            button_erase.Name = "button_erase";
+            button_erase.Size = new Size(105, 88);
+            button_erase.TabIndex = 15;
+            button_erase.UseVisualStyleBackColor = true;
+            button_erase.Click += button_erase_Click;
+            // 
+            // button_0
+            // 
+            button_0.Cursor = Cursors.Hand;
+            button_0.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button_0.Location = new Point(127, 534);
+            button_0.Name = "button_0";
+            button_0.Size = new Size(105, 88);
+            button_0.TabIndex = 14;
+            button_0.Text = "0";
+            button_0.UseVisualStyleBackColor = true;
+            button_0.Click += button_0_Click;
+            // 
+            // button_decimal
+            // 
+            button_decimal.Cursor = Cursors.Hand;
+            button_decimal.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button_decimal.Location = new Point(11, 534);
+            button_decimal.Name = "button_decimal";
+            button_decimal.Size = new Size(105, 88);
+            button_decimal.TabIndex = 13;
+            button_decimal.Text = ".";
+            button_decimal.UseVisualStyleBackColor = true;
+            button_decimal.Click += button_decimal_Click;
+            // 
+            // button_equal
+            // 
+            button_equal.Cursor = Cursors.Hand;
+            button_equal.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button_equal.Location = new Point(243, 142);
+            button_equal.Name = "button_equal";
+            button_equal.Size = new Size(221, 78);
+            button_equal.TabIndex = 18;
+            button_equal.Text = "=";
+            button_equal.UseVisualStyleBackColor = true;
+            button_equal.Click += button_equal_Click;
+            // 
+            // clear_button
+            // 
+            clear_button.Cursor = Cursors.Hand;
+            clear_button.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            clear_button.Location = new Point(12, 142);
+            clear_button.Name = "clear_button";
+            clear_button.Size = new Size(221, 78);
+            clear_button.TabIndex = 17;
+            clear_button.Text = "Clear";
+            clear_button.UseVisualStyleBackColor = true;
+            clear_button.Click += clear_button_Click;
+            // 
+            // label_first_number
+            // 
+            label_first_number.Location = new Point(324, 4);
+            label_first_number.Name = "label_first_number";
+            label_first_number.Size = new Size(88, 38);
+            label_first_number.TabIndex = 19;
+            label_first_number.Text = "100";
+            label_first_number.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // label_operator
+            // 
+            label_operator.Location = new Point(407, 11);
+            label_operator.Name = "label_operator";
+            label_operator.Size = new Size(59, 25);
+            label_operator.TabIndex = 19;
+            label_operator.Text = "+";
+            label_operator.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // CalculatorFrm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(474, 640);
+            Controls.Add(label_operator);
+            Controls.Add(label_first_number);
+            Controls.Add(button_equal);
+            Controls.Add(clear_button);
+            Controls.Add(button_plus);
+            Controls.Add(button_erase);
+            Controls.Add(button_0);
+            Controls.Add(button_decimal);
+            Controls.Add(button_minus);
+            Controls.Add(button_3);
+            Controls.Add(button_2);
+            Controls.Add(button_1);
+            Controls.Add(button_mult);
+            Controls.Add(button_6);
+            Controls.Add(button_5);
+            Controls.Add(button_4);
+            Controls.Add(button_div);
+            Controls.Add(button_9);
+            Controls.Add(button_8);
+            Controls.Add(button_7);
+            Controls.Add(main_textBox);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "CalculatorFrm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "SimpleCalc";
+            TopMost = true;
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox main_textBox;
+        private Button button_7;
+        private Button button_8;
+        private Button button_9;
+        private Button button_div;
+        private Button button_mult;
+        private Button button_6;
+        private Button button_5;
+        private Button button_4;
+        private Button button_minus;
+        private Button button_3;
+        private Button button_2;
+        private Button button_1;
+        private Button button_plus;
+        private Button button_erase;
+        private Button button_0;
+        private Button button_decimal;
+        private Button button_equal;
+        private Button clear_button;
+        private Label label_first_number;
+        private Label label_operator;
+        private ToolTip toolTip_read_number;
+    }
+}
---
+++ CalculatorFrm.cs
@@ -0,0 +1,322 @@
+using System.Text;
+
+namespace SimpleCalc
+{
+    public partial class CalculatorFrm : Form
+    {
+        public CalculatorFrm()
+        {
+            InitializeComponent();
+        }
+
+        void clear_all()
+        {
+            this.main_textBox.Text = "0";
+            this.label_first_number.Text = "";
+            this.label_operator.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            clear_all();
+        }
+
+        private void clear_button_Click(object sender, EventArgs e)
+        {
+            clear_all();
+        }
+
+
+        private void button_0_Click(object sender, EventArgs e)
+        {
+            numeric_calc("0");
+        }
+
+        private void button_1_Click(object sender, EventArgs e)
+        {
+            numeric_calc("1");
+        }
+        private void button_2_Click(object sender, EventArgs e)
+        {
+            numeric_calc("2");
+        }
+
+        private void button_3_Click(object sender, EventArgs e)
+        {
+            numeric_calc("3");
+        }
+
+        private void button_4_Click(object sender, EventArgs e)
+        {
+            numeric_calc("4");
+        }
+
+        private void button_5_Click(object sender, EventArgs e)
+        {
+            numeric_calc("5");
+        }
+
+        private void button_6_Click(object sender, EventArgs e)
+        {
+            numeric_calc("6");
+        }
+
+        private void button_7_Click(object sender, EventArgs e)
+        {
+            numeric_calc("7");
+        }
+
+        private void button_8_Click(object sender, EventArgs e)
+        {
+            numeric_calc("8");
+        }
+
+        private void button_9_Click(object sender, EventArgs e)
+        {
+            numeric_calc("9");
+
+        }
+        void numeric_calc(string button_number)
+        {
+            if (this.main_textBox.Text == "0")
+            {
+                this.main_textBox.Text = button_number;
+            }
+            else
+            {
+                this.main_textBox.Text += button_number;
+            }
+        }
+
+
+        void operator_buttons(string op_type)
+        {
+            if (this.main_textBox.Text != "")
+            {
+                this.label_first_number.Text = this.main_textBox.Text;
+            }
+            this.label_operator.Text = op_type;
+            this.main_textBox.Text = "";
+
+
+        }
+
+        private void button_plus_Click(object sender, EventArgs e)
+        {
+            operator_buttons("+");
+        }
+
+
+        private void button_minus_Click(object sender, EventArgs e)
+        {
+            operator_buttons("-");
+        }
+
+        private void button_mult_Click(object sender, EventArgs e)
+        {
+            operator_buttons("x");
+        }
+
+        private void button_div_Click(object sender, EventArgs e)
+        {
+            operator_buttons("/");
+        }
+
+        private void button_equal_Click(object sender, EventArgs e)
+        {
+            string firstNumberStr = this.label_first_number.Text;
+            string secondNumberStr = this.main_textBox.Text;
+
+            try
+            {
+                double result = PerformOperation(firstNumberStr, secondNumberStr, this.label_operator.Text);
+
+                this.main_textBox.Text = result.ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Make sure that the operators are accompanied by numerical values.", ":/");
+            }
+
+        }
+
+        private void button_decimal_Click(object sender, EventArgs e)
+        {
+            if (this.main_textBox.Text.Contains(".") == true)
+            {
+                return;
+            }
+            else
+            {
+                numeric_calc(".");
+            }
+        }
+        private double PerformOperation(string firstNumberStr, string secondNumberStr, string operatorStr)
+        {
+            double fn = double.Parse(firstNumberStr);
+            double sn = double.Parse(secondNumberStr);
+            double result = 0;
+
+            switch (operatorStr)
+            {
+                case "+":
+                    result = fn + sn;
+                    break;
+                case "-":
+                    result = fn - sn;
+                    break;
+                case "x":
+                    result = fn * sn;
+                    break;
+                case "/":
+                    result = fn / sn;
+                    break;
+
+                default:
+                    result = 0;
+                    break;
+            }
+
+            return result;
+        }
+
+        private void button_erase_Click(object sender, EventArgs e)
+        {
+            EraseOne();
+        }
+
+        private void EraseOne()
+        {
+            string currentText = this.main_textBox.Text;
+
+            if (!string.IsNullOrEmpty(currentText))
+            {
+                this.main_textBox.Text = currentText.Substring(0, currentText.Length - 1);
+            }
+        }
+
+        private void main_textBox_MouseHover(object sender, EventArgs e)
+        {
+
+            toolTip_read_number.AutoPopDelay = 30000;
+            toolTip_read_number.InitialDelay = 1000;
+            toolTip_read_number.ReshowDelay = 500;
+
+            string result = this.main_textBox.Text;
+            double number;
+            string digits = "It was not possible to read this number.";
+
+
+
+            try
+            {
+                double.TryParse(result, out number);
+                digits = NumberToWords(number);
+
+                toolTip_read_number.Show(digits, main_textBox);
+            }
+            catch (Exception)
+            {
+                toolTip_read_number.Show(digits, main_textBox);
+            }
+        }
+
+        private void main_textBox_MouseLeave(object sender, EventArgs e)
+        {
+            toolTip_read_number.Hide(main_textBox);
+        }
+
+        private void main_textBox_TextChanged(object sender, EventArgs e)
+        {
+            int textBoxLenght = main_textBox.Text.Length;
+
+            if (textBoxLenght > 100)
+            {
+                main_textBox.Text = main_textBox.Text[..100];
+            }
+        }
+
+        static string NumberToWords(double number)
+        {
+            int integerPart = (int)Math.Floor(number);
+            int decimalPart = (int)Math.Round((number - integerPart) * 100);
+
+            StringBuilder words = new StringBuilder();
+
+            if (integerPart == 0 && decimalPart == 0)
+                return "zero";
+
+            if (integerPart < 0)
+                words.Append("minus ");
+
+            words.Append(NumberToWords(Math.Abs(integerPart)));
+
+            if (decimalPart > 0)
+            {
+                words.Append(" point ");
+                words.Append(NumberToWords(decimalPart));
+            }
+
+            return words.ToString();
+        }
+
+        static string NumberToWords(int number)
+        {
+            string[] units = { "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            string[] teens = { "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+            string[] tens = { "", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+
+            if (number == 0)
+                return "zero";
+
+            if (number < 0)
+                return "menos " + NumberToWords(Math.Abs(number));
+
+            StringBuilder words = new StringBuilder();
+
+            if ((number / 1000000000) > 0)
+            {
+                words.Append(NumberToWords(number / 1000000000) + " billion ");
+                number %= 1000000000;
+            }
+
+            if ((number / 1000000) > 0)
+            {
+                words.Append(NumberToWords(number / 1000000) + " million ");
+                number %= 1000000;
+            }
+
+            if ((number / 1000) > 0)
+            {
+                words.Append(NumberToWords(number / 1000) + " thousand ");
+                number %= 1000;
+            }
+
+            if ((number / 100) > 0)
+            {
+                words.Append(NumberToWords(number / 100) + " hundred ");
+                number %= 100;
+            }
+
+            if (number > 0)
+            {
+                if (words.Length > 0)
+                    words.Append("and ");
+
+                if (number < 10)
+                    words.Append(units[number]);
+                else if (number < 20)
+                    words.Append(teens[number - 11]);
+                else
+                {
+                    words.Append(tens[number / 10]);
+                    if ((number % 10) > 0)
+                        words.Append(" " + units[number % 10]);
+                }
+            }
+
+            return words.ToString();
+        }
+
+    }
+}
